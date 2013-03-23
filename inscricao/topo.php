c380dcf7da3a7d73d07843418afe083cf962f983
Augusto <carmolim@Augusto.local>
2013-03-22 23:59:43 -0300

    topo
    topo

---
+++ inscricao/topo.php
@@ -0,0 +1,48 @@
+<?php
+?>
+
+	<!DOCTYPE html>
+	<html xmlns="http://www.w3.org/1999/xhtml" >
+	<head>
+	<title><?php echo $titulo; ?> - Bom Retiro de Inverno</title>
+
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta property="fb:admins" content="1063748681"/>
+		<meta property="og:title" content="Bom Retiro de Inverno"/>
+		<meta property="og:type" content="cause"/>
+		<meta property="og:url" content="http://www.bomretirodeinverno.com.br/"/>
+		<meta property="og:image" content="http://www.bomretirodeinverno.com.br/img/logo-fb.png"/>
+		<meta property="og:site_name" content="Bom Retiro de Inverno"/>
+		<meta property="og:description" content="Do dia 29/05 ao 02/06 acontecerá a segunda edição do Bom Retiro de Inverno, com a presença do Pr. Joni Roger de Oliveira e do grupo 7MUS, dirigido por Jader Santos cuidando do nosso louvor! Com certeza será um Bom Retiro! :)"/>
+		<meta http-equiv="Content-Style-Type" content="text/css" />
+	
+		<meta content='pt' http-equiv='content-language'/>
+		<meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+		
+		<link rel="STYLESHEET" type="text/css" href="../css/global.css" />
+		
+		<style>
+		
+			#content
+			{	
+				width: 565px;
+				margin-left: auto;
+				margin-right: auto;
+			}
+			
+			#topo
+			{
+				text-align: center;
+			}
+			
+			#box
+			{
+				background: rgb(255, 255, 255);
+				background: rgba(255, 255, 255, .2);
+				padding: 20px;
+			}
+			
+		</style>	
+	</head>
